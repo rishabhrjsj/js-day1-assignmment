9adb2151b51081f26e917530a89cc086960c287a
Rishabh Jaiswal <rishabhrjjaiwal@gmail.com>
2025-12-09 03:47:47 +0000

    tast 3

---
+++ setup.js
@@ -0,0 +1,10 @@
+// Steps to setup GitHub + VS Code + Node.js using Codespaces
+
+// 1. Create GitHub Account
+// 2. Create a new repository
+// 3. Open Code → Codespaces → Create codespace
+// 4. This opens VS Code online automatically
+// 5. Node.js and Git already installed in Codespace
+// 6. Create JS files (intro.js, setup.js)
+// 7. Run programs with: node filename.js
+// 8. Commit & push directly from VS Code online
